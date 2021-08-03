b18fa5988650a7158a30e1a93ecdc9becf898390
julienboussard <jb4365@columbia.edu>
2021-08-03 11:30:33 +0200

    Update run.py

--- localization_pipeline/run.py
+++ localization_pipeline/run.py
@@ -6,28 +6,40 @@ from localizer import LOCALIZER
 from denoiser import Denoise
 from merge_results import get_merged_arrays
 
-bin_file = '/media/cat/julien/nick_drift/different_preprocessings/data_standardized_registered/standardized_unregistered.bin'
-residual_file = '/media/cat/julien/test_final_loc_code/residuals/residual.bin'
+
+### Change paths to your data here
+bin_file = 'standardized_data.bin'
 dtype_input = 'float32'
-fname_spike_train = "/media/cat/julien/nick_drift/spt_yass.npy"
-fname_templates = "/media/cat/julien/nick_drift/templates_yass.npy"
-geom_path = "/media/cat/julien/nick_drift/geom.npy"
-denoiser_weights = '/media/cat/julien/nn_np2/denoise.pt'
+fname_spike_train = "spt.npy"
+geom_path = "geom_np2.npy"
+n_channels = np.load(geom_path).shape[0]
+
+denoiser_weights = '/pretrained_denoiser/denoise.pt'
 denoiser_min = 42 ## Goes with the weights
+
 n_batches = 1000
 len_recording = 1000
 sampling_rate = 30000
-n_channels = 384
-fname_out = '/media/cat/julien/test_final_loc_code/residuals/residual.bin'
-dtype_out = 'float32'
-dtype_in = 'float32'
-n_batches = 1000
+
+### If templates are already computed, input the path here
+fname_templates = "templates.npy"
+### Otherwise, uncomment and run 
+# fname_templates = None
+# localizer_obj = LOCALIZER(bin_file, residual_file, dtype_input, fname_spike_train, fname_templates, geom_path, denoiser_weights, denoiser_min)
+# localizer_obj.get_templates()
+# fname_templates = "templates.npy"
+# np.save(fname_templates, localizer_obj.templates)
 
 ##### COMPUTE RESIDUALS 
 '''
 This is necessary for denoising spikes and removing collisions
 It needs to be ran only once per datasets, as residual.bin file can be stored and reused 
 '''
+
+fname_out = 'residuals/residual.bin'
+dtype_out = 'float32'
+dtype_in = 'float32'
+
 residual_obj = RESIDUAL(bin_file,
                  fname_templates,
                  fname_spike_train,
@@ -40,8 +52,11 @@ residual_obj = RESIDUAL(bin_file,
                  dtype_out)
         
 
-residual_obj.compute_residual('/media/cat/julien/test_final_loc_code/residuals/')
+residual_obj.compute_residual('residuals/')
 residual_obj.save_residual()
+
+### If residuals are already computed, input the location here
+residual_file = 'residuals/residual.bin'
 
 
 ##### LOCALIZE SPIKES 
